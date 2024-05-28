b101bfd1e74c32865e0667124db3a8f6d37532cf
alexandredomi <149201812+alexandredomi@users.noreply.github.com>
2024-05-28 00:19:48 -0300

    add menu mobile

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnMenuOpen = document.querySelector("#btnMenuOpen")
+const btnMenuClose = document.querySelector("#btnMenuClose")
+
+const menuMobile = document.querySelector(".menuMobile")
+
+btnMenuOpen.addEventListener("click", function() {
+    menuMobile.classList.add("flex")
+})
+btnMenuClose.addEventListener("click", function() {
+    menuMobile.classList.remove("flex")
+})
+
+console.log("deu certo")

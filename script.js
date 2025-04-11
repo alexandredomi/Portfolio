dc85ec0cfe5b1e17c83f9c9eaac28773ffeaf47c
Alexandre Domingues <149201812+alexandredomi@users.noreply.github.com>
2025-04-11 20:26:00 -0300

    add

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const btnMenuOpen = document.querySelector("#btnMenuOpen")
 const btnMenuClose = document.querySelector("#btnMenuClose")
 const btnCurriculo = document.querySelector("#btn-download")
 const btnCloseLinkMenuMobile = document.querySelectorAll("#closeLinkMenuMobile");
-
 const menuMobile = document.querySelector(".menuMobile")
 
+//abrir e fechar menu mobile 
 btnMenuOpen.addEventListener("click", function() {
     menuMobile.classList.add("flex")
 })
 btnMenuClose.addEventListener("click", function() {
     menuMobile.classList.remove("flex")
 })
+//fechando menu mobile ao clicar no links de direcionamento 
 btnCloseLinkMenuMobile.forEach((btn) => {
     btn.addEventListener("click", function() {
         menuMobile.classList.remove("flex");
@@ -19,5 +20,3 @@ btnCloseLinkMenuMobile.forEach((btn) => {
 btnCurriculo.addEventListener('click', ()=>{
     alert('curriculo baixado')
 })
-
- 
